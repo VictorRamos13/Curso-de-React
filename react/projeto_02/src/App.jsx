62bc05702c85744476a70ec562b2f6e8b7811065
Victor Hordones Ramos <victorhordonesramos38@gmail.com>
2026-01-13 19:27:29 -0300

    feat: projeto02 - envio de dados por components

---
+++ react/projeto_02/src/App.jsx
@@ -0,0 +1,29 @@
+import './App.css';
+import Fofocas from './components/Fofocas';
+import Header from './components/Header';
+
+const fofocas = [
+    {
+        id: 1,
+        titulo: 'Últimas Fofocas do Dia',
+        manchete: 'Mauro é visto em encontro misterioso',
+        texto: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex debitis voluptatem hic autem officia nesciunt aut cupiditate sint nihil perferendis, velit dolor eos atque repudiandae voluptatum nisi quis ut culpa?',
+    },
+    {
+      id: 2,
+        titulo: 'Fofoca Quentinha',
+        manchete: 'Aula de React tem surpresa inesperada',
+        texto: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex debitis voluptatem hic autem officia nesciunt aut cupiditate sint nihil perferendis, velit dolor eos atque repudiandae voluptatum nisi quis ut culpa?',
+    },
+];
+
+function App() {
+    return (
+        <>
+            <Header />
+            <Fofocas fofocas={fofocas} />
+        </>
+    );
+}
+
+export default App;
